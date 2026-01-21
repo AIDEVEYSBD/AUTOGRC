20c3cb653d1dbe26e7f6c6d7bd9d239b02555891
Jai Verma <jai.verma2@in.ey.com>
2026-01-21 19:47:46 +0530

    Update actions.tsx

--- src/app/(platform)/overview/actions.tsx
+++ src/app/(platform)/overview/actions.tsx
@@ -1,7 +1,7 @@
 "use server"
 
 import { getControlsByDomain, type ControlInDomain } from "@/lib/overview.queries"
-export const dynamic = "force-dynamic"
+
 export async function fetchControlsByDomain(
   masterFrameworkId: string,
   domainName: string
